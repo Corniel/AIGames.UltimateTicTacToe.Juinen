f4d33a800f3756a7f0f490d0165746d78fb14c75
Corniel Nobel <mail@corniel.nl>
2016-01-23 15:16:05 +0100

    Updated move generator.

--- src/AIGames.UltimateTicTacToe.Juinen/DecisionMaking/MoveGenerator.cs
+++ src/AIGames.UltimateTicTacToe.Juinen/DecisionMaking/MoveGenerator.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AIGames.UltimateTicTacToe.Juinen.DecisionMaking
 {
 	public class MoveGenerator
 	{
-		//public IEnumerator<ushort[]> GetMoves(ushort[] macroboard, )
+		public IEnumerable<int[]> GetMoves(int[] macro, bool oToMove, int active)
+		{
+			var options = oToMove ? TinyBoard.MovesO : TinyBoard.MovesX;
+
+			if (active != 9)
+			{
+				var tiny = macro[active];
+				var moveCount = TinyBoard.MoveCount[tiny];
+				if (moveCount != 0)
+				{
+					for (var move = 0; move < 9; move ++)
+					{
+						var response = options[tiny, move];
+
+						if (response == TinyBoard.Empty)
+						{
+							continue;
+						}
+						var copy = new int[10];
+						Array.Copy(macro, copy, 10);
+						copy[active] = response;
+						yield return copy;
+					}
+					yield break;
+				}
+			}
+			for (var a = 0; a < 9; a++)
+			{
+				if (TinyBoard.MoveCount[macro[a]] != 0)
+				{
+					foreach (var response in GetMoves(macro, oToMove, a))
+					{
+						yield return response;
+					}
+				}
+			}
+		}
 	}
 }
